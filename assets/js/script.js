458b26a7b47c301105880a528eedb5d5677ede69
gerdesjasonl <gerdesjasonl@gmail.com>
2025-01-11 17:39:53 -0500

    This is a contact button that opens a modal pop-up form. small tweaks to mobile look and general spacing to accomodate new button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 const boxEl = document.querySelector('.box')
-
+const contactModalEl = document.getElementById('contactModal');
+const contactBtnEl = document.querySelector('contactBtn');
+const contCloseBtnEl = document.getElementById('contCloseBtn');
+const contSendBtnEl = document.getElementById('contSendBtn');
+const contNameEl = document.getElementById('contNameInput')
+const contEmailEl = document.getElementById('contactEmailInput');
+const contMessageEl = document.getElementById('contactMessageInput');
+const errorEl = document.getElementById('errorMessage');
+const contFormEl = document.getElementById('contactForm')
 
 function chambord(){
     document.getElementById("chambord").innerHTML = "<img src='./assets/images/Chambord (1).jpg' alt='Château de Chambord' style='width: 300px; height: 300px; padding: -9px;'/>"
@@ -12,3 +20,39 @@ function chenonceau(){
 function bretagne (){
    document.getElementById("bretagne").innerHTML = "<img src='./assets/images/Bretagne (1).jpg' alt='Château des Ducs de Bretagne' style='width: 300px; height: 300px; padding: -9px;'/>"
 }
+// This is where the Modals scripts are stored
+
+const handleContact = function () {
+    contactModalEl.style.display = 'block';
+}
+
+contactBtnEl.addEventListener('click', handleContact);
+
+
+contCloseBtnEl.addEventListener('click', function() {
+    contactModalEl.modal('hide');
+});
+
+const visitorMessage = {
+    Email: contEmailEl.value.trim(),
+    Message: contMessageEl.value.trim(),
+};
+
+const messageArray = [];
+
+const handleSubmit = function (){
+    // event.preventDefault ();
+    if (contEmailEl.value === "") {
+        errorEl.textContent = "Please complete the form."
+        } else if (contMessageEl.value === "") { 
+            errorEl.textContent = "Please complete the form."
+            } else {
+                return visitorMessage
+                }; 
+    messageArray.push(visitorMessage);
+    localStorage.setItem("messages", JSON.stringify(messageArray));
+};
+
+contSendBtnEl.addEventListener('click', handleSubmit);
+
+// // This is the bottom of the modals section
