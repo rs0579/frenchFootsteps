fbb96be697c06667a1bded2594d3f5656d136daa
rs0579 <raheemsenegal@outlook.com>
2025-01-13 21:13:24 +0100

    JS mods

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
+
 // Element variables from html/////////////
+
+const boxEl = document.querySelector('.box')
 const contactModalEl = document.getElementById('contactModal');
 const contactBtnEl = document.getElementById('contactBtn');
 const contCloseBtnEl = document.getElementById('contCloseBtn');
@@ -11,34 +14,23 @@ const refModalEl = document.getElementById('refModal');
 const refBtnEl = document.getElementById('refBtn');
 const addResBtnEl = document.getElementById('addResBtn')
 const addResModalEl = document.getElementById('addResModalEl')
-<<<<<<< HEAD
-
-=======
-// const state = document.getAttribute("data-state")
-// const expo = document.getAttribute("onclick")
->>>>>>> 1d24ac309516f5aad61fba5ab63f7f77f683afcc
+const state = document.getElementById('chambord').getAttribute('data-hidden')
 
 
 
-function chambord() {
+
+function chambord(event){
     document.getElementById("chambord").innerHTML = "<img src='./assets/images/Chambord (1).jpg' alt='Château de Chambord' style='width: 300px; height: 300px; padding: -9px;'/>"
-}
-
-<<<<<<< HEAD
-function chenonceau() {
-    document.getElementById("chenonceau").innerHTML = "<img src='./assets/images/Chenonceau (1).jpg' alt='Château de Chenonceau' style='width: 300px; height: 300px; padding: -9px;'/>"
-=======
-boxEl.addEventListener('click', function(event){
-    element = event.target
-    if (state === "hidden"){
-        element.innerHTML = element.expo
-        element.dataset.state = 'visible'
-    } else{
-        element.textContent = ""
-        element.setAttribute("data-state", "hidden")
+    const element = event.target.chambord
+    if (element === state){
+        setAttribute('style', 'display: none;')
+    }else { 
+        setAttribute('style','display: block')
+        
+    
     }
-
-})
+   
+}
 
 
 function chenonceau(){document.getElementById("chenonceau").innerHTML = "<img src='./assets/images/Chenonceau (1).jpg' alt='Château de Chenonceau' style='width: 300px; height: 300px; padding: -9px;'/>"
@@ -49,29 +41,20 @@ function bretagne (){document.getElementById("bretagne").innerHTML = "<img src='
 function foix (){document.getElementById("foix").innerHTML = "<img src='./assets/images/Foix (1).jpg' alt='Château de Foix' style='width: 300px; height: 300px; padding: -9px;'/>"}
 
 function chenonceau(){ document.getElementById("chenonceau").innerHTML = "<img src='./assets/images/Chenonceau (1).jpg' alt='Château de Chenonceau' style='width: 300px; height: 300px; padding: -9px;'/>"
->>>>>>> 1d24ac309516f5aad61fba5ab63f7f77f683afcc
 }
 
-function bretagne() {
-    document.getElementById("bretagne").innerHTML = "<img src='./assets/images/Bretagne (1).jpg' alt='Château des Ducs de Bretagne' style='width: 300px; height: 300px; padding: -9px;'/>"
+function bretagne (){ document.getElementById("bretagne").innerHTML = "<img src='./assets/images/Bretagne (1).jpg' alt='Château des Ducs de Bretagne' style='width: 300px; height: 300px; padding: -9px;'/>"
 }
-<<<<<<< HEAD
-function foix() {
-    document.getElementById("foix").innerHTML = "<img src='./assets/images/Foix (1).jpg' alt='Château de Foix' style='width: 300px; height: 300px; padding: -9px;'/>"
-}
-function tours() {
-=======
 function foix (){ document.getElementById("foix").innerHTML = "<img src='./assets/images/Foix (1).jpg' alt='Château de Foix' style='width: 300px; height: 300px; padding: -9px;'/>"
 
  }
  function tours (){
->>>>>>> 1d24ac309516f5aad61fba5ab63f7f77f683afcc
     document.getElementById("tours").innerHTML = "<img src='./assets/images/Tours.jpg' alt='Château de Tours' style='width: 300px; height: 300px; padding: -9px;'/>"
-}
-function versailles() {
+ }
+ function versailles (){
     document.getElementById("versailles").innerHTML = "<img src='./assets/images/palace.jpg' alt='Palace de Versailles' style='width: 300px; height: 300px; padding: -9px;'/>"
-}
-
+ }
+ 
 // This is where the Modals scripts are grouped/////////////
 
 // This is the beginning of script for the contact form modal///////////
@@ -91,17 +74,9 @@ function versailles() {
 
 const messageArray = [];
 console.log("Wehave a console!")
-<<<<<<< HEAD
-const visitorMessageEl = {
-    Name: contNameEl.value.trim(),
-    Email: contEmailEl.value.trim(),
-    Message: contMessageEl.value.trim(),
-};
-=======
 
->>>>>>> 1d24ac309516f5aad61fba5ab63f7f77f683afcc
 
-const handleSubmit = function (event) {
+const handleSubmit = function (event){
     console.log("handleSubmit success")
     event.preventDefault();
     const visitorMessageEl = {
@@ -111,32 +86,25 @@ const handleSubmit = function (event) {
         };
     if (contEmailEl.value.trim() === "") {
         alertError();
-    } if (contMessageEl.value.trim() === "") {
-        alertError();
-    } if (contNameEl.value.trim() === "") {
-        alertError();
-    } else {
-        console.log(visitorMessageEl);
-        messageArray.push(visitorMessageEl);
-        localStorage.setItem("messages", JSON.stringify(messageArray));
-        console.log("You have received a new message!");
-    };
+        } if (contMessageEl.value.trim() === "") { 
+            alertError();
+            } if (contNameEl.value.trim() === ""){
+                alertError();
+                } else {
+                    console.log(visitorMessageEl);
+                    messageArray.push(visitorMessageEl);
+                    localStorage.setItem("messages", JSON.stringify(messageArray));
+                    console.log ("You have received a new message!");
+                };
 };
 
 const alertError = function () {
     const errorEl = document.createElement('p');
     contFormEl.appendChild(errorEl);
-    errorEl.textContent = "Please complete the form.";
-}
+       errorEl.textContent = "Please complete the form.";
+    }
 
-<<<<<<< HEAD
-contSendBtnEl.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log("sendbtn works")
-})
-=======
 contFormEl.addEventListener('submit', handleSubmit)
->>>>>>> 1d24ac309516f5aad61fba5ab63f7f77f683afcc
 
 
 // (e)=>{handleSubmit(e)}
